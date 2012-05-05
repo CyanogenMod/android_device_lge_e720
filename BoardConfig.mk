02181b1d372e7892d9a4a1c572eaf33527d1f1f0
Ricardo Cerqueira <github@cerqueira.org>
2012-05-05 18:11:45 +0100

    e720: Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,62 @@
+USE_CAMERA_STUB := true
+BOARD_USE_FROYO_LIBCAMERA := true
+
+# inherit from the proprietary version
+-include vendor/lge/e720/BoardConfigVendor.mk
+
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_CPU_ABI := armeabi
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_CPU_ABI := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := e720
+
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=alessi
+BOARD_KERNEL_BASE := 0x12800000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00440000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0c800000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0bd60000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/lge/e720/kernel
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+TARGET_PROVIDES_LIBAUDIO := true
+TARGET_PROVIDES_LIBRIL := true
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# to enable the GPS HAL
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := e720
+# AMSS version to use for GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+BOARD_EGL_CFG := device/lge/e720/configs/egl.cfg
+
+TARGET_SPECIFIC_HEADER_PATH := device/lge/e720/include
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+BOARD_WLAN_DEVICE := bcm4325
+WIFI_DRIVER_FW_STA_PATH         := "/system/etc/wl/rtecdc.bin"
+WIFI_DRIVER_FW_AP_PATH          := "/system/etc/wl/rtecdc-apsta.bin"
+WIFI_DRIVER_MODULE_NAME         := "wireless"
+WIFI_DRIVER_MODULE_PATH         := "/system/lib/modules/wireless.ko"
+WIFI_DRIVER_MODULE_ARG          := "firmware_path=/etc/wl/rtecdc.bin nvram_path=/etc/wl/nvram.txt config_path=/data/misc/wifi/config"
+WPA_SUPPLICANT_VERSION          := VER_0_6_X
+WIFI_DRIVER_HAS_LGE_SOFTAP      := true
+BOARD_WEXT_NO_COMBO_SCAN       := true
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+
+BOARD_FM_DEVICE := bcm4325
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
+TARGET_OTA_ASSERT_DEVICE := alessi,e720
